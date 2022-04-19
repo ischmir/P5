8217f94a943fb774f08eb3ac890af4343cf73ce6
FreddieEgedal <frederikejensen@gmail.com>
2022-04-19 13:00:21 +0200

    ændringr

--- js_filer/frederik.js
+++ js_filer/frederik.js
@@ -26,3 +26,15 @@ function boligFunction() {
 }
 boligFunction()
 //Bolig dropdown Slut//
+
+//GOD DAG TEKST
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 18) {
+  greeting = "Besked er modtaget, Hav en god dag!";
+} else {
+  greeting = "Besked er modtaget, Hav en god aften!";
+}
+
+document.getElementById("alert").innerHTML = greeting;
