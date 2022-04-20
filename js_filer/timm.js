fd50487b0eb73efb7f797ff0cc8de1aa27752575
nicklashg <74595173+nicklashg@users.noreply.github.com>
2022-04-20 10:53:38 +0200

    updates

--- js_filer/timm.js
+++ js_filer/timm.js
@@ -28,3 +28,11 @@ navSlide();
 
 //JAVASCRIPT NAV SLUT
 
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
